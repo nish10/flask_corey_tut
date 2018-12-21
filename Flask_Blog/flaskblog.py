4c713cc14b9da0e75e6cf8ac751a0c88ac321cb7
nish <nishant.raj334@gmail.com>
2018-12-21 15:18:41 +0530

    login form

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -38,16 +38,20 @@ def register():
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
-    return render_template('register.html',
-                           title='Register',
-                           form=form)
+    return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html',
-                       title='Login',
-                       form=form)
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
