51ce7d3af7270d883a1fa03b6bb9637b7366ae6b
nish <nishant.raj334@gmail.com>
2018-12-21 17:19:54 +0530

    update changes

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import  RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
-
-
 app.config['SECRET_KEY'] = 'c21f402817d5f58c1e1ac7321371d292'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    username = db.column(db.String(20), unique=True, nullable=False)
+    email = db.email(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='defualt.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}
+
+
 
 
 posts = [
