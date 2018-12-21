3c354df6e73d226bfe5754e52ffa0ede037caa86
nish <nishant.raj334@gmail.com>
2018-12-21 15:25:51 +0530

    Add existing file

--- Flask_Blog/forms.py
+++ Flask_Blog/forms.py
@@ -18,9 +18,6 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(),
-                                       Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(),
                                     Email()])
